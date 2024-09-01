e60a231547f01d013d6bc0a9f2a3908afc16e384
sandybhatta <105071143+sandybhatta@users.noreply.github.com>
2024-09-01 11:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,26 @@ let arr = [1, 2, 3, 4];
 
 // Initial promise that resolves after 3 seconds
 new Promise((resolve) => {
-
-    resolve(arr);
+  resolve(arr);
 })
   .then((arr) => {
-    // Filter even numbers after the initial array is resolved
     return new Promise((resolve) => {
       let arr1 = arr.filter((item) => item % 2 === 0);
       setTimeout(() => {
-        op.innerText = arr1.join(', '); // Display even numbers after 1 second
+        
+        op.innerText = arr1.join(', ');
         resolve(arr1);
       }, 1000);
     });
   })
   .then((arr1) => {
-    // Multiply the filtered even numbers by 2
     return new Promise((resolve) => {
       setTimeout(() => {
         let multipliedArr = arr1.map((item) => item * 2);
-        op.innerText = multipliedArr.join(', '); // Update output with multiplied numbers after 2 seconds
+       
+        op.innerText = multipliedArr.join(', ');
         resolve(multipliedArr);
       }, 2000);
     });
-  })
+  });
   
